7ce8a048b9c88b4a15d0db47f655b8ea3a141153
yangzhenyu <51511793@qq.com>
2018-06-11 10:48:48 +0800

    add
    add files

---
+++ eb/src/main/java/com/xyd/eb/EbApplication.java
@@ -0,0 +1,12 @@
+package com.xyd.eb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbApplication.class, args);
+	}
+}
---
+++ eb/src/test/java/com/xyd/eb/EbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xyd.eb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
